b8f1104cfd27a93e95a5eb52e5932aa35ad3eece
Cheikh NDIAYE <cheikhn414@gmail.com>
2025-04-24 22:36:11 +0000

    [Back][Partie 2]: add User controller

---
+++ back/src/main/java/com/alten/alten_shop/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.alten.alten_shop.controller;
+
+import com.alten.alten_shop.dto.UserRequest;
+import com.alten.alten_shop.dto.UserResponse;
+import com.alten.alten_shop.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public UserResponse register(@RequestBody UserRequest userRequest) {
+        return userService.register(userRequest);
+    }
+}
