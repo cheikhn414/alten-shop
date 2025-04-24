6ffb22d1287b13abc4d9bd80dc09560cb2a49f7f
Cheikh NDIAYE <cheikhn414@gmail.com>
2025-04-24 20:25:13 +0000

    initial commit

---
+++ back/src/main/java/com/alten/alten_shop/AltenShopApplication.java
@@ -0,0 +1,13 @@
+package com.alten.alten_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltenShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltenShopApplication.class, args);
+	}
+
+}
