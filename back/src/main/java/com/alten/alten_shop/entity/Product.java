e2ac1e97ebbd563129fdbd472b446a738b292826
Cheikh NDIAYE <cheikhn414@gmail.com>
2025-04-24 21:00:46 +0000

    [Back][Partie 1]: add Product entity

---
+++ back/src/main/java/com/alten/alten_shop/entity/Product.java
@@ -0,0 +1,190 @@
+package com.alten.alten_shop.entity;
+
+import jakarta.persistence.*;
+import java.math.BigInteger;
+
+enum InventoryStock {
+    INSTOCK, LOWSTOCK, OUTOFSTOCK;
+}
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String code;
+
+    String name;
+
+    String description;
+
+    String image;
+
+    String category;
+
+    Double price;
+
+    Integer quantity;
+
+    String internalReference;
+
+    Integer shellId;
+
+    InventoryStock inventoryStatus;
+
+    Integer rating;
+
+    BigInteger createdAt;
+
+    BigInteger updatedAt;
+
+    public Product() {
+    }
+
+    public Product(String code, String name, String description, String image, String category, Double price, Integer quantity, String internalReference, Integer shellId, InventoryStock inventoryStatus, Integer rating, BigInteger createdAt, BigInteger updatedAt) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+        this.internalReference = internalReference;
+        this.shellId = shellId;
+        this.inventoryStatus = inventoryStatus;
+        this.rating = rating;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Product(Long id, String code, String name, String description, String image, String category, Double price, Integer quantity, String internalReference, Integer shellId, InventoryStock inventoryStatus, Integer rating, BigInteger createdAt, BigInteger updatedAt) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+        this.internalReference = internalReference;
+        this.shellId = shellId;
+        this.inventoryStatus = inventoryStatus;
+        this.rating = rating;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getInternalReference() {
+        return internalReference;
+    }
+
+    public void setInternalReference(String internalReference) {
+        this.internalReference = internalReference;
+    }
+
+    public Integer getShellId() {
+        return shellId;
+    }
+
+    public void setShellId(Integer shellId) {
+        this.shellId = shellId;
+    }
+
+    public InventoryStock getInventoryStatus() {
+        return inventoryStatus;
+    }
+
+    public void setInventoryStatus(InventoryStock inventoryStatus) {
+        this.inventoryStatus = inventoryStatus;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public BigInteger getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(BigInteger createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public BigInteger getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(BigInteger updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
