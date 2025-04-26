6e97ae13c66b957ccaf92d816017891cf2492ef9
Cheikh NDIAYE <cheikhn414@gmail.com>
2025-04-26 16:40:08 +0000

    [Front][Partie 1]: Afficher un badge indiquant la quantité de produits dans le panier

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,18 +1,22 @@
 import {
-  Component,
+  Component, inject,
 } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { SplitterModule } from 'primeng/splitter';
 import { ToolbarModule } from 'primeng/toolbar';
 import { PanelMenuComponent } from "./shared/ui/panel-menu/panel-menu.component";
+import {CartService} from "./products/data-access/cart.service";
+import {BadgeModule} from "primeng/badge";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
   standalone: true,
-  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent],
+  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent, BadgeModule],
 })
 export class AppComponent {
   title = "ALTEN SHOP";
+
+  readonly cartService = inject(CartService);
 }
